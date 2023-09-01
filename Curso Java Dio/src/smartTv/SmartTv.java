91f4591572a82582e9706a07f9529a83e818fc2f
raimundonetoo <89netocomercial@gmail.com>
2023-09-01 11:32:03 -0300

    Modificação SmartTv

--- Curso Java Dio/src/smartTv/SmartTv.java
+++ Curso Java Dio/src/smartTv/SmartTv.java
@@ -1,9 +1,50 @@
 package smartTv;
 
 public class SmartTv {
-		boolean ligada = false;
-		int canal = 0;
-		int volume = 0;
-	
+	boolean ligada = false;
+	int canal = 0;
+	int volume = 0;
+
+	public void AumentarVolume() {
+		volume++;
+		if (volume <= 100) {
+			volume = 100;
+			System.out.println("Volume máximo " + volume);
+		}
 	}
 
+	public void DiminuirVolume() {
+		volume--;
+		if (volume <= 0) {
+			volume = 0;
+			System.out.println("Volume " + volume);
+		}
+	}
+
+	public void LigarTv() {
+		ligada = true;
+	}
+
+	public void DesligarTv() {
+		ligada = false;
+	}
+
+	public void AlterarCanal(int newCanal) {
+		canal = newCanal;
+		if (canal <= 0) {
+			canal = 0;
+			System.out.println("Erro...Canal invalido");
+		}
+	}
+
+	public void AlternarCanalUp() {
+		canal++;
+	}
+
+	public void AlternarCanalDown() {
+		canal--;
+		if (canal <= 0) {
+			canal = 0;
+			System.out.println("Erro...Canal invalido");
+	}
+}}
--- Curso Java Dio/src/smartTv/Usuario.java
+++ Curso Java Dio/src/smartTv/Usuario.java
@@ -8,6 +8,12 @@ public class Usuario {
 		System.out.println("A tv está ligada ? " + (SmartTv.ligada ? "Sim" : "Não"));
 		System.out.println("O Canal Atual é "+ SmartTv.canal);
 		System.out.println("O volume atual da Tv é " + SmartTv.volume);
+		SmartTv.AlterarCanal(1);
+		System.out.println("O Canal Atual é "+ SmartTv.canal);
+		SmartTv.AlternarCanalDown();
+		SmartTv.AlternarCanalDown();
+		SmartTv.AlternarCanalDown();
+		System.out.println("O Canal Atual é "+ SmartTv.canal);
 	}
 
 }
